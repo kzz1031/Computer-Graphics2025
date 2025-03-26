dd849344dde15133f9cf5990c9f250218477ccc2
kzz <Zhuanz@MacBook-Air-2.local>
2025-03-26 09:36:42 +0800

    init

---
+++ build/CMakeFiles/a1.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/Zhuanz/Documents/Graphics/starter1/src/camera.cpp" "CMakeFiles/a1.dir/src/camera.cpp.o" "gcc" "CMakeFiles/a1.dir/src/camera.cpp.o.d"
+  "/Users/Zhuanz/Documents/Graphics/starter1/src/curve.cpp" "CMakeFiles/a1.dir/src/curve.cpp.o" "gcc" "CMakeFiles/a1.dir/src/curve.cpp.o.d"
+  "/Users/Zhuanz/Documents/Graphics/starter1/src/main.cpp" "CMakeFiles/a1.dir/src/main.cpp.o" "gcc" "CMakeFiles/a1.dir/src/main.cpp.o.d"
+  "/Users/Zhuanz/Documents/Graphics/starter1/src/parse.cpp" "CMakeFiles/a1.dir/src/parse.cpp.o" "gcc" "CMakeFiles/a1.dir/src/parse.cpp.o.d"
+  "/Users/Zhuanz/Documents/Graphics/starter1/src/starter1_util.cpp" "CMakeFiles/a1.dir/src/starter1_util.cpp.o" "gcc" "CMakeFiles/a1.dir/src/starter1_util.cpp.o.d"
+  "/Users/Zhuanz/Documents/Graphics/starter1/src/surf.cpp" "CMakeFiles/a1.dir/src/surf.cpp.o" "gcc" "CMakeFiles/a1.dir/src/surf.cpp.o.d"
+  "/Users/Zhuanz/Documents/Graphics/starter1/src/vertexrecorder.cpp" "CMakeFiles/a1.dir/src/vertexrecorder.cpp.o" "gcc" "CMakeFiles/a1.dir/src/vertexrecorder.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
